a9dc38f210053022f4e04e2f8060352fcb60f736
Zefir Kirilov <contact@zefir.pro>
2025-07-27 16:53:31 +0300

    create indices

---
+++ src/registry/index.ts
@@ -0,0 +1,5 @@
+export {RegistryClient} from "./RegistryClient.js";
+export {RegistryDependencyType} from "./RegistryDependencyType.js";
+export {RegistryPackage} from "./RegistryPackage.js";
+export {RegistryReleaseChannel} from "./RegistryReleaseChannel.js";
+export {RegistryVersion} from "./RegistryVersion.js";
