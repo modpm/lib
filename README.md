e95f5e5c0dcafe9d535fbf1f5fdb3ba66d010453
Zefir Kirilov <contact@zefir.pro>
2025-07-29 13:07:31 +0300

    Changed SPDX licence identifier and README link to GPL-3.0

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 Copyright © 2025 Zefir Kirilov.
 
-This project is licensed under the terms of the [LGPL-3.0](https://www.gnu.org/licenses/lgpl-3.0.en.html) licence.
+This project is licensed under the terms of the [GPL-3.0](https://www.gnu.org/licenses/gpl-3.0.en.html) licence.
--- src/registry/RegistryClient.ts
+++ src/registry/RegistryClient.ts
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 import {RegistryPackage} from "./RegistryPackage.js";
 import {RegistryReleaseChannel} from "./RegistryReleaseChannel.js";
 import {RegistryVersion} from "./RegistryVersion.js";
