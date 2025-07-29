a80e5b1650871ef8dad2bd2634ea405e89168234
Zefir Kirilov <contact@zefir.pro>
2025-07-29 13:13:45 +0300

    updated indices

---
+++ src/events/index.ts
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+export {TypedEventListener} from "./TypedEventListener.js";
+export {TypedEventListenerObject} from "./TypedEventListenerObject.js";
+export {TypedEventTarget} from "./TypedEventTarget.js";
