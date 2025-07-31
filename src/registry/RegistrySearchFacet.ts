09ba02626b82ad30f5edff14c559c37a6dee9aaf
Zefir Kirilov <contact@zefir.pro>
2025-07-31 10:00:02 +0300

    allow comparison operators on `color` search facet

--- src/registry/RegistrySearchFacet.ts
+++ src/registry/RegistrySearchFacet.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 export type RegistrySearchFacet =
     `project_type${":" | "=" | "!="}${"mod" | "modpack" | "resourcepack" | "shader" | "plugin" | "datapack"}`
-    | `${"categories" | "versions" | "title" | "author" | "project_id" | "license" | "color"}${":" | "=" | "!="}${string}`
-    | `${"follows" | "downloads" | "created_timestamp" | "modified_timestamp"}${":" | "=" | "!=" | ">=" | ">" | "<=" | "<"}${number}`;
+    | `${"categories" | "versions" | "title" | "author" | "project_id" | "license"}${":" | "=" | "!="}${string}`
+    | `${"follows" | "downloads" | "color" | "created_timestamp" | "modified_timestamp"}${":" | "=" | "!=" | ">=" | ">" | "<=" | "<"}${number}`;
