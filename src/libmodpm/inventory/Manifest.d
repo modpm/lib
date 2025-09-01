b0cb03ab9524cf74ba42e16abd0df8ab19ad13d6
Zefir Kirilov <contact@zefir.pro>
2025-09-01 22:09:52 +0300

    manifest class

---
+++ src/libmodpm/inventory/Manifest.d
@@ -0,0 +1,11 @@
+module libmodpm.inventory.Manifest;
+
+/**
+ * Represents the manifest of installed packages in a given scope.
+ */
+public final class Manifest {
+    /**
+     * Manifest version.
+     */
+     public static enum int ver = 0;
+}
--- src/libmodpm/inventory/package.d
+++ src/libmodpm/inventory/package.d
@@ -1,3 +1,4 @@
 module libmodpm.inventory;
 
+public import libmodpm.inventory.Manifest;
 public import libmodpm.inventory.InstalledPackage;
